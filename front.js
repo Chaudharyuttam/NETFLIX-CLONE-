eaed18791d19aa4aa28558c7b84419c7d1d4a1aa
Uttam Chaudhary <chaudharyuttam269@gmail.com>
2025-08-06 02:14:29 +0530

    Initial commit

--- front.js
+++
@@ -1,6 +0,0 @@
-function changetext() {
-const para1 = document.getElementsByClassName('para1');
-para1.textContent = "Uttam Chaudhary Website";
-}
-const para1 = document.getElementsByClassName('para1');
-para1.addEventListener('click', changetext);
